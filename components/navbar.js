c229b22610d796c2eb1a009e93d84d9bbec46376
ldnelson16 <ldnelson16@gmail.com>
2023-07-16 15:17:48 -0400

    Framing navbar

---
+++ components/navbar.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+
+export default function() {
+  return (
+    <div className="">
+      This is where navbar goes
+    </div>
+  )
+}
