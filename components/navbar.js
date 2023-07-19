30cfd2a377b4e0853fc529c8338f242d3dea3c85
ldnelson16 <ldnelson16@gmail.com>
2023-07-18 21:21:25 -0400

    Learning React

--- components/navbar.js
+++ components/navbar.js
@@ -1,13 +1,24 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/layout.module.css';
+import styles from '../styles/navbar.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-export default function() {
+function button(link,linktext,dropdown,dropdowncontents,dropdownlinks){
   return (
-    <div className="">
-      This is where navbar goes
+    <div className={styles.option}>
+      <Link href={link}>{linktext}</Link>
+      <div class
+      {dropdown?(<div className="dropdown">)+dropdowncontents.map((content,index) => (<Link href={dropdownlinks[index]}>{content}</Link>)</div>):(<></>)}
+    </div>
+  );
+}
+
+
+export default function Navbar() {
+  return (
+    <div className={styles.bar}>
+      {button("/","Option 1")}
     </div>
   )
 }
