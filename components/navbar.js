6851f5b117eaf75333bda24d8913b954e82a3926
ldnelson16 <ldnelson16@gmail.com>
2023-07-19 22:20:41 -0400

    Still same thing

--- components/navbar.js
+++ components/navbar.js
@@ -5,11 +5,11 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import React from 'react';
 
-function button(link,linktext,dropdown,dropdowncontents,dropdownlinks){
+function button(link,linktext,dropdown,dropdowncontents){
   return (
     <div className={styles.option}>
       <Link href={link}>&nbsp;&nbsp;{dropdown?<>{linktext}&#9660;</>:<>{linktext}</>}&nbsp;&nbsp;</Link>
-      {dropdown?(<div className={styles.dropdown}>{dropdowncontents.map((content,index)=><Link href={dropdownlinks[index]}>{content}</Link>)}</div>):(<></>)}
+      {dropdown?(<><div className={styles.dropdown}>{dropdowncontents.map((content)=>(<><Link href={content[1]}>{content[0]}</Link><>{content[2]?(<div className={styles.subdropdown}>{content[3].map((con)=>(<Link href={con[1]}>{con[0]}</Link>))}</div>):(<></>)}</></>))}</div><></></>):(<></>)}
     </div>
   );
 }
@@ -18,8 +18,8 @@ function button(link,linktext,dropdown,dropdowncontents,dropdownlinks){
 export default function Navbar() {
   return (
     <div className={styles.bar}>
-      {button("/","CFB Recruiting",true,["Recruits Scraper","Create Your Own Composite"],["/","/"])}
-      {button("/","Option 2",false)}
+      {button("/","CFB Recruiting",true,[["Recruits Scraper","/"],["Create Your Own Composite","/",true,["HI","ii"]]])}
+      {button("/posts/first-post","Search Algorithms",false)}
     </div>
   )
 }
