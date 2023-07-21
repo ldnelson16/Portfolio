536ba3c4aa71f01e472b2ae454f2ef32db9caf1b
ldnelson16 <ldnelson16@gmail.com>
2023-07-20 23:04:29 -0400

    working on navbar

--- components/navbar.js
+++ components/navbar.js
@@ -5,11 +5,24 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import React from 'react';
 
+function navlogo(){
+  return (
+    <div className={styles.navlogo}><Image priority src="/../public/images/bannerlogo.png" width={300} height={70} alt="Banner Logo with text 'LDNELSON16' and 'Portfolio'"></Image></div>
+  );
+}
+
 function button(link,linktext,dropdown,dropdowncontents){
   return (
     <div className={styles.option}>
       <Link href={link}>&nbsp;&nbsp;{dropdown?<>{linktext}&#9660;</>:<>{linktext}</>}&nbsp;&nbsp;</Link>
-      {dropdown?(<><div className={styles.dropdown}>{dropdowncontents.map((content)=>(<><Link href={content[1]}>{content[0]}</Link><>{content[2]?(<div className={styles.subdropdown}>{content[3].map((con)=>(<Link href={con[1]}>{con[0]}</Link>))}</div>):(<></>)}</></>))}</div><></></>):(<></>)}
+      {dropdown?
+        (<>
+          <div className={styles.dropdown}>
+            {dropdowncontents.map((content)=>(<><Link href={content[1]}>{content[0]}</Link>{content[2]?(<div className={styles.subdropdown}>{content[3].map((con)=>(<Link href={con[1]}>{con[0]}</Link>))}</div>):(<></>)}</>))}
+          </div>
+        </>):
+        (<></>)
+      }
     </div>
   );
 }
@@ -18,8 +31,10 @@ function button(link,linktext,dropdown,dropdowncontents){
 export default function Navbar() {
   return (
     <div className={styles.bar}>
-      {button("/","CFB Recruiting",true,[["Recruits Scraper","/"],["Create Your Own Composite","/",true,["HI","ii"]]])}
+      {navlogo()}
+      {button("/","CFB Recruiting",true,[["Recruits Scraper","/"],["Create Your Own Composite","/",true,[["HI","/"]]]])}
       {button("/posts/first-post","Search Algorithms",false)}
+      {button("/aboutme","About Me",true,[["Portfolio","/"],["About Me","/"],["GitHub Links","/"]])}
     </div>
   )
 }
