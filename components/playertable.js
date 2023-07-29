058227e02e5a91b3f31935e764fca75c8cb273f4
Luke Nelson <ldnelson16@gmail.com>
2023-07-28 21:13:50 -0400

    Changing playertable

--- components/playertable.js
+++ components/playertable.js
@@ -5,7 +5,48 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import * as playerdata from '../data/classof2025data.json';
 
-function headerCell(){
+void function changeDates(value,index){
+    console.log("Changing dates to "+value);
+
+}
+
+function DatesDropdown(dates){
+    const handleDropSelection = () => {
+        console.log("CHanged");
+    }
+    return(
+        <select className={styles.datedropdown} onChange={handleDropSelection}>
+            {dates.map((date,i)=>(<option value={i}>{date}</option>))}
+        </select>
+    );
+}
+
+const HeaderCell = ({}) => {
+    return(
+        <div className={styles.headerCell} id="header">
+            <div className={styles.playerInfo}>
+                <em>Player Info</em>
+            </div>
+            <div className={styles.ron3}>
+                <em>ON3</em>
+            </div>
+            <div className={styles.r247}>
+                <em>247</em>
+            </div>
+            <div className={styles.respn}>
+                <em>ESPN</em>
+            </div>
+            <div className={styles.rrivals}>
+                <em>Rivals</em>
+            </div>
+            <div className={styles.commitInfo}>
+                <em>Commit Status</em>
+            </div>
+        </div>
+    );
+} 
+
+/*function headerCell(){
     return(
         <div className={styles.headerCell} id="header">
             <div className={styles.playerInfo}>
@@ -30,7 +71,7 @@ function headerCell(){
     );
 }
 
-function playerCell(data,key){
+const playerCell = ({data,key,index}) => {
     return(
         <div className={styles.playerCell} id={key}>
             <div className={styles.playerInfo}>
@@ -45,16 +86,47 @@ function playerCell(data,key){
                 </div>
             </div>
             <div className={styles.ron3}>
-                {data["ON3 Rating"][0]}
+                {data["ON3 Rating"][index]}
             </div>
             <div className={styles.r247}>
-                {data["247 Rating"][0]}
+                {data["247 Rating"][index]}
             </div>
             <div className={styles.respn}>
-                {data["ESPN Rating"][0]}
+                {data["ESPN Rating"][index]}
             </div>
             <div className={styles.rrivals}>
-                {data["Rivals Rating"][0]}
+                {data["Rivals Rating"][index]}
+            </div>
+            {data["Commit Status"]==false?<div className={styles.commitInfo}>Uncommitted</div>:<div className={styles.commitInfo}><b>{data["Commit Status"]}</b></div>}
+        </div>
+    );
+};*/
+
+function playerCell(data,key,index){
+    return(
+        <div className={styles.playerCell} id={key}>
+            <div className={styles.playerInfo}>
+                <div className={styles.posInfo}>
+                    {data["Pos"]}
+                </div>
+                <div className={styles.locationInfo}>
+                    {data["City"]+", "+data["State"]}
+                </div>
+                <div className={styles.nameInfo}>
+                    {data["name"]}
+                </div>
+            </div>
+            <div className={styles.ron3}>
+                {data["ON3 Rating"][index]}
+            </div>
+            <div className={styles.r247}>
+                {data["247 Rating"][index]}
+            </div>
+            <div className={styles.respn}>
+                {data["ESPN Rating"][index]}
+            </div>
+            <div className={styles.rrivals}>
+                {data["Rivals Rating"][index]}
             </div>
             {data["Commit Status"]==false?<div className={styles.commitInfo}>Uncommitted</div>:<div className={styles.commitInfo}><b>{data["Commit Status"]}</b></div>}
         </div>
@@ -63,11 +135,16 @@ function playerCell(data,key){
 
 export default function Playertable(){
     let data = playerdata["players"];
+    let dates = playerdata["dates"];
+    const handleDropSelection = () => {
+        console.log('Dropdown changed to '+date+'!');
+      };
     return(
         <div className={styles.playerTable}>
+            {DatesDropdown(dates)}
             {console.log("DATA processing from JSON file")}
-            {headerCell()}
-            {data.map((datum,i)=>playerCell(datum,i))}
+            <HeaderCell></HeaderCell>
+            {data.map((datum,i)=>playerCell(datum,i,0))}
         </div>
     )    
 }
--- pages/cfbrecruiting/sortable.js
+++ pages/cfbrecruiting/sortable.js
@@ -13,7 +13,6 @@ export default function Page(){
             <Navbar></Navbar>
             <h1>Heading</h1>
             <Playertable></Playertable>
-            
         </>
     );
 }
